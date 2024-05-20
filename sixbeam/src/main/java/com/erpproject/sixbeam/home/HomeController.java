deb0c5fb5044b959432aab1a07acdfc3c90ca2b4
LeeSang90 <LeeSang90@naver.com>
2024-05-20 14:23:15 +0900

    sql url 주소 변경

--- sixbeam/src/main/java/com/erpproject/sixbeam/home/HomeController.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/home/HomeController.java
@@ -34,8 +34,8 @@ public class HomeController {
     public String sixbeam_main(Model model) {
         List<EstimateEntity> estimateEntities =estimateService.getList();
         List<OrinPutEntity> orinPutEntities=orinPutService.getList();
-        String url = "http://localhost:8000/sales-summary";
-        String url2 = "http://localhost:8000/input-summary";
+        String url = "http://13.209.99.109:8000/sales-summary";
+        String url2 = "http://13.209.99.109:8000/input-summary";
         GrapeForm saleGrapeForm = grapeService.getGrapePredictions(url);
         GrapeForm inputGrapeForm = grapeService.getGrapePredictions(url2);
         model.addAttribute("saleGrapeForm", saleGrapeForm);
